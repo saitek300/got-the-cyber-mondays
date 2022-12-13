d1f8c11b843ae56bcf930b3f966ef5cfc1f7cd0b
saitek <saitek300@gmail.com>
2022-12-12 22:15:59 -0800

    route bug fix

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,9 +7,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  try{
+  try {
     const productData = await Product.findAll({
-      include: [{model:Category}, {model:Tag}],
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -21,12 +21,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  try{
+  try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{model:Category}, {model:Tag}],
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
     });
-    if(!productData) {
-      res.status(404).json({message:'no product found with that id!'})
+    if (!productData) {
+      res.status(404).json({ message: 'no product found with that id!' })
     }
     res.status(200).json(productData);
   } catch (err) {
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,15 +3,15 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/',async (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{model: Product}],
+      include: [{ model: Product, through: ProductTag, as:'product_tags'}],
     });
     res.status(200).json(tagData);
-  } catch (err) {
+  } catch (err) { console.log(err);
     res.status(500).json(err);
   }
 });
@@ -19,13 +19,13 @@ router.get('/',async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  try{
+  try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{model: Product}],
+      include: [{ model: Product, through: ProductTag }],
     });
 
-    if(!tagData) {
-      res.status(404).json({message:'no tag found with that id!'})
+    if (!tagData) {
+      res.status(404).json({ message: 'no tag found with that id!' })
     }
 
     res.status(200).json(tagData);
